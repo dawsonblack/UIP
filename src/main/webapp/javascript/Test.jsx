7abe2769d1377a68b26c69741f0e62ac0dd29908
JohnsonRobertE <JohnsonRobertE809@gmail.com>
2023-08-07 14:35:40 -0400

    Did not mean to have this in main/ Landing page dev

---
+++ src/main/webapp/javascript/Test.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+export default function Test() {
+  //For testing functions and such
+  return (
+    <div>
+      <div>
+        <button>Dont click</button>
+      </div>
+    </div>
+  );
+}

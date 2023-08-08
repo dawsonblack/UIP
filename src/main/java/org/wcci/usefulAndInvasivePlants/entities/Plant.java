44dd41db76381a0dced48f19b2e6d53b1913963e
dawsonblack <134227563+dawsonblack@users.noreply.github.com>
2023-08-08 09:56:42 -0400

    fixed file name issue

--- src/main/java/org/wcci/usefulAndInvasivePlants/entities/Plant.java
+++ src/main/java/org/wcci/usefulAndInvasivePlants/entities/Plant.java
@@ -1,21 +1,13 @@
 package org.wcci.usefulAndInvasivePlants.entities;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+////import com.fasterxml.jackson.core.type.TypeReference;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 

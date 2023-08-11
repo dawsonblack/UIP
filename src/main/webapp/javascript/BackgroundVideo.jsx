04fce773605f560c4f13dcc60fc93c20d583bd19
Abdikarim Farah <abdikarimfarah12@gmail.com>
2023-08-11 15:04:27 -0400

    pushing

--- src/main/webapp/javascript/BackgroundVideo.jsx
+++ src/main/webapp/javascript/BackgroundVideo.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { CSSTransition } from 'react-transition-group';
-import './BackgroundVideo.css';
 
 const videos = [
   "flower1.mp4",
@@ -10,96 +8,54 @@ const videos = [
 ];
 
 class BackgroundVideo extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        currentVideoIndex: 0,
-      };
-      this.videoRef = React.createRef();
-    }
-  
-    componentDidMount() {
-      this.playVideo();
-    }
-  
-    handleVideoEnd = () => {
-      this.setState(
-        (prevState) => ({
-          currentVideoIndex: (prevState.currentVideoIndex + 1) % videos.length,
-        }),
-        this.playVideo
-      );
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentVideoIndex: 0,
     };
-  
-    playVideo() {
-      const video = this.videoRef.current;
-      video.src = videos[this.state.currentVideoIndex];
-      video.load();
-      video.play();
-      video.onended = this.handleVideoEnd;
-    }
-  
-    render() {
-      return (
-        <div className="background-video-container">
-          <CSSTransition
-            in={true}
-            appear={true}
-            timeout={1000}
-            classNames="fade"
-            key={this.state.currentVideoIndex}
-          >
-            <video
-              ref={this.videoRef}
-              autoPlay
-              muted
-              className="background-video"
-              key={this.state.currentVideoIndex}
-            />
-          </CSSTransition>
-        </div>
-      );
-    }
+    this.videoRef = React.createRef();
   }
-  
-  export default BackgroundVideo;
-// class BackgroundVideo extends Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         currentVideoIndex: 0,
-//       };
-//       this.videoRef = React.createRef();
-//     }
 
-//     componentDidMount() {
-//       this.playVideo();
-//     }
+  componentDidMount() {
+    this.playVideo();
+  }
 
-//     handleVideoEnd = () => {
-//       this.setState((prevState) => ({
-//         currentVideoIndex: (prevState.currentVideoIndex + 1) % videos.length,
-//       }), this.playVideo);
-//     };
+  componentDidUpdate() {
+    this.playVideo();
+  }
 
-//     playVideo() {
-//       const video = this.videoRef.current;
-//       video.src = videos[this.state.currentVideoIndex];
-//       video.load();
-//       video.play();
-//       video.onended = this.handleVideoEnd;
-//     }
+  playVideo() {
+    const video = this.videoRef.current;
+    video.src = videos[this.state.currentVideoIndex];
+    video.load();
+    video.play();
+    video.onended = this.handleVideoEnd;
+  }
 
-//     render() {
-//       return (
-//         <video
-//           ref={this.videoRef}
-//           autoPlay
-//           muted
-//           style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: -1 }}
-//         />
-//       );
-//     }
-//   }
+  handleVideoEnd = () => {
+    this.setState((prevState) => ({
+      currentVideoIndex: (prevState.currentVideoIndex + 1) % videos.length,
+    }));
+  };
 
-//   export default BackgroundVideo;
+  render() {
+    return (
+      <video
+        ref={this.videoRef}
+        autoPlay
+        muted
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          zIndex: -2,
+        }}
+      />
+    );
+  }
+}
+
+export default BackgroundVideo;

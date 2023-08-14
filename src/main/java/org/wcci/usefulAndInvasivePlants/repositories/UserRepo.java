3b97462ee3084b11f7ca44626a4ae34efc8e222b
Siman Farah <zfarah23@gmail.com>
2023-08-14 10:10:05 -0400

    POST mapping for User entity

--- src/main/java/org/wcci/usefulAndInvasivePlants/repositories/UserRepo.java
+++ src/main/java/org/wcci/usefulAndInvasivePlants/repositories/UserRepo.java
@@ -10,16 +10,16 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepo extends CrudRepository<User, Long> {
 
 
-    // @Query(name = "select * from user", nativeQuery = true)
-    // public List<User> getUsers();
+    @Query(name = "select * from user", nativeQuery = true)
+    public List<User> getUsers();
 
 
-    // @Query(name = "select * from user where id=?1 and name=?2")
-    // public User getUserByIdAndUserName(long id, String userName);
+    @Query(name = "select * from user where id=?1 and name=?2")
+    public User getUserByIdAndUserName(long id, String userName);
 
 
-    // @Query(name = "select * from user where id=?1")
-    // public User getUserById(long id);
+    @Query(name = "select * from user where id=?1")
+    public User getUserById(long id);
 
 
 
--- src/main/java/org/wcci/usefulAndInvasivePlants/restControllers/UserRestController.java
+++ src/main/java/org/wcci/usefulAndInvasivePlants/restControllers/UserRestController.java
@@ -1,41 +1,52 @@
 package org.wcci.usefulAndInvasivePlants.restControllers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.wcci.usefulAndInvasivePlants.entities.Plant;
 import org.wcci.usefulAndInvasivePlants.entities.User;
 import org.wcci.usefulAndInvasivePlants.services.PlantService;
 
-
-
 @RestController
 public class UserRestController {
-
-    //field injection through @autowired
-    @Autowired
+    public static final String LIST_ALL_USERS = "listAllUsers";
     private PlantService plantService;
 
-    @GetMapping ("api/users/{id}")
-    public Optional<User> getUser(@PathVariable long id){
-        return plantService.getUserById(id); 
+    public UserRestController(@Autowired PlantService plantService) {
+        this.plantService = plantService;
     }
 
+    @GetMapping("api/users/{id}")
+    public Optional<User> getUser(@PathVariable long id) {
+        return plantService.getUserById(id);
+    }
 
     @GetMapping("api/users")
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return plantService.getUsers();
     }
 
     @DeleteMapping("api/users/{id}")
-    public void deleteById(@PathVariable long id){
+    public void deleteById(@PathVariable long id) {
         plantService.deleteUserByID(id);
     }
 
+    @PostMapping("/api/users")
+    public EntityModel<User> newUser(@RequestBody final User user) {
+        return EntityModel.of(plantService.addNewUser(user),
+                linkTo(methodOn(UserRestController.class).getUser(User.getId())).withSelfRel(),
+                linkTo(methodOn(UserRestController.class).getUser()).withRel(LIST_ALL_USERS));
+    }
 
 }

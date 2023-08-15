af35a4a32e504848b58ae2c19c2679f9f3d92f28
dawsonblack <134227563+dawsonblack@users.noreply.github.com>
2023-08-15 13:03:19 -0400

    added password hashing

--- src/main/webapp/javascript/Test.jsx
+++ src/main/webapp/javascript/Test.jsx
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from "react";
 
 export default function Test() {
-  //For testing functions and such
+  async function sha256(message) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder().encode(message);                    
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string                  
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+  }
+
+  async function hash() {
+    const password = document.getElementById("password").value;
+    console.log(password);
+    const hashedPassword = await sha256(password);
+    console.log(hashedPassword);
+  }
+  
   return (
     <div>
       <div>
-        <button>Dont click</button>
+        <input type="text" id="password" />
+        <button onClick={hash}>Submit</button>
       </div>
     </div>
   );

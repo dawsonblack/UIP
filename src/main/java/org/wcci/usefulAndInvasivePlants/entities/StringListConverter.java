6ab6dd85a1f3f5df6372921b6ef60de7749d7993
JohnsonRobertE <JohnsonRobertE809@gmail.com>
2023-08-17 09:41:30 -0400

    s

--- src/main/java/org/wcci/usefulAndInvasivePlants/entities/StringListConverter.java
+++ src/main/java/org/wcci/usefulAndInvasivePlants/entities/StringListConverter.java
@@ -3,6 +3,9 @@ package org.wcci.usefulAndInvasivePlants.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,8 +16,14 @@ import jakarta.persistence.Converter;
 @Converter
 public class StringListConverter implements AttributeConverter<List<String>, String> {
 
+    final private static Logger logger = LoggerFactory.getLogger(StringListConverter.class);
+
     @Override
     public String convertToDatabaseColumn(List<String> attribute) {
+        if (attribute == null) {
+            logger.warn("Received a null list");
+            return "";
+        }
         try {
             return new ObjectMapper().writeValueAsString(attribute);
         } catch (JsonProcessingException e) {
@@ -24,6 +33,10 @@ public class StringListConverter implements AttributeConverter<List<String>, Str
 
     @Override
     public List<String> convertToEntityAttribute(String dbData) {
+        if (dbData == null) {
+            logger.trace("Received a null string");
+            return new ArrayList<>(0);
+        }
         try {
             return new ObjectMapper().readValue(
                     dbData, new TypeReference<List<String>>() {
